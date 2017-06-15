96967f4ad47ea3d38a3e4726bf15bd4d67349f81
boothy008 <markbooth94@gmail.com>
2017-06-15 22:35:00 +0100

    Basic Boat Sway

---
+++ Assets/Scripts/Boat/BoatRocker.cs
@@ -0,0 +1,54 @@
+﻿using Assets;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoatRocker : MonoBehaviour {
+
+    // >0 = pushing right. <0 pushing left
+    public float pushingForce = 0;
+    public float loseAngle = 25;
+
+    public Weather currentWeather;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    //Adjusts the boats balance
+    void adjustBalance(float wind, float push)
+    {
+        float pushBoat = push + wind;
+
+        Vector3 euler = transform.eulerAngles;
+        euler.z -= (pushBoat * Time.deltaTime);
+
+        transform.eulerAngles = euler;
+    }
+
+    //Checks the boats balance
+    void checkBalance()
+    {
+        Vector3 euler = transform.rotation.eulerAngles;
+
+        if(euler.z > loseAngle && euler.z < (360 - loseAngle))
+        {
+            euler.z = 0;
+            transform.rotation = Quaternion.Euler(euler);
+        }
+    }
+
+	// Update is called once per frame
+	void Update () {
+
+        //Get player pushing force
+        pushingForce = (currentWeather.GetMaxWindSpeed() * 2) * Input.GetAxis(Tags.Horizontal);
+
+        //Adjust the boats balance
+        adjustBalance(currentWeather.GetWindSpeed(), pushingForce);
+
+        //Check if we are still okay
+        checkBalance();
+	}
+}
---
+++ Assets/Scripts/Weather System/Weather.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weather : MonoBehaviour
+{
+
+    //Wind speed pushing the boat
+    public float windSpeed = 0;
+    public float maxWindSpeed = 15;
+
+    public float windDifference = 0;
+    public float difficultyMultiplier = 1;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    //Add random wind
+    void AddWind(float addWindSpeed)
+    {
+        windDifference += (addWindSpeed * Time.deltaTime);
+    }
+
+    //Add a Gust of wind
+    void AddGust()
+    {
+        int rand = Random.Range(0, 2);
+
+        if (rand == 0)
+        {
+            windDifference = -(maxWindSpeed * difficultyMultiplier);
+        }
+        else
+        {
+            windDifference = (maxWindSpeed * difficultyMultiplier);
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        int rand = Random.Range(0, 100);
+        //Add wind speed difference
+        if (rand > 62 && rand < 66)
+        {
+            AddGust();
+        }
+
+        AddWind(Random.Range(-maxWindSpeed, maxWindSpeed));
+
+        //Clamp windDifferect stopping it from going one direction to much
+        windDifference = Mathf.Clamp(windDifference, -(maxWindSpeed * difficultyMultiplier), (maxWindSpeed * difficultyMultiplier));
+
+        //Update windSpeed;
+        windSpeed += (windDifference * Time.deltaTime);
+
+        //Clamp wind speed
+        windSpeed = Mathf.Clamp(windSpeed, -maxWindSpeed, maxWindSpeed);
+    }
+
+
+    public float GetMaxWindSpeed()
+    {
+        return maxWindSpeed;
+    }
+
+    public float GetWindSpeed()
+    {
+        return windSpeed;
+    }
+}
