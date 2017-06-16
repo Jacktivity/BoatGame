eba288532da540d6c55f223d99bea61ee231e6fb
John <jshone94@yahoo.com>
2017-06-16 23:13:54 +0100

    UI Further Update

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class PauseMenu : MonoBehaviour {
 
     public GameObject pausemenucanvas;
+    private bool paused;
     // Use this for initialization
     void Start ()
     {
         pausemenucanvas.SetActive(false);
+        Time.timeScale = 1f;
     }
 	
 	// Update is called once per frame
@@ -17,10 +19,18 @@ public class PauseMenu : MonoBehaviour {
 		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
         {
             Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-            if(touchDeltaPosition.x > 3)
+            if(touchDeltaPosition.x > 26 && !paused)
             {
                 pausemenucanvas.SetActive(true);
                 Time.timeScale = 0f;
+                paused = true;
+            }
+
+            if (touchDeltaPosition.x < -26 && paused)
+            {
+                pausemenucanvas.SetActive(false);
+                Time.timeScale = 1f;
+                paused = false;
             }
         }
 	}
