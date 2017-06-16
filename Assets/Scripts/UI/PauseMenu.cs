0914eb0ad19590b2805ddfb5b0bb5b7d98a8696a
JohnShone94 <jshone94@yahoo.com>
2017-06-16 22:39:24 +0100

    UI#1

---
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public GameObject pausemenucanvas;
+    // Use this for initialization
+    void Start ()
+    {
+        pausemenucanvas.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        {
+            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
+            if(touchDeltaPosition.x > 3)
+            {
+                pausemenucanvas.SetActive(true);
+                Time.timeScale = 0f;
+            }
+        }
+	}
+
+
+}
