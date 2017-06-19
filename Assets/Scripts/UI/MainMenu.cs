84e33a75ea9bf06aa19dbc74b29d18107128ce4c
John <jshone94@yahoo.com>
2017-06-19 15:48:02 +0100

    next update

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -42,7 +42,7 @@ public class MainMenu : MonoBehaviour
 
     public void btnStoryMode()
     {
-        SceneManager.LoadScene("BoatGame");
+        SceneManager.LoadScene("MainGame");
     }
 
     public void btnSurvivalMode()
