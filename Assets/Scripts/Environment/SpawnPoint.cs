77a47d45bb498db0f6b7594ec9da442ac26819fc
Jacktivity <jh140695@gmail.com>
2017-06-19 21:26:46 +0100

    Fishies
    I have started adding fishies

--- Assets/Scripts/Environment/SpawnPoint.cs
+++ Assets/Scripts/Environment/SpawnPoint.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class SpawnPoint : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public GameObject greenFish;
+    public GameObject purpleFish;
+    public GameObject theFish;
+    int randFish;
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+       Vector3 spawnPos = transform.position;
+        if (Random.Range(0, 100) >= 99)
+        {
+            randFish = Random.Range(0, 20);
+            if (randFish < 15)
+            {
+                theFish = greenFish;
+            }
+            else if (randFish >= 15)
+            {
+                theFish = purpleFish;
+            }
+            Instantiate(theFish, spawnPos, transform.rotation);
+        }
+    }
 }
--- Assets/Scripts/Environment/fishScript.cs
+++ Assets/Scripts/Environment/fishScript.cs
@@ -10,7 +10,13 @@ public class fishScript : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        gameObject.GetComponent<Transform>().position = new Vector3(gameObject.GetComponent<Transform>().position.x - 0.2f, gameObject.GetComponent<Transform>().position.y);
+        if (gameObject.GetComponent<Transform>().position.x <= -23)
+        {
+            Destroy(gameObject);
+        }
+       
+    }
 }
